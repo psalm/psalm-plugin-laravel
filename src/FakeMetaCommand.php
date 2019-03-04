73c85279092cfa52c82199cb4ca692eb038b033c
Matthew Brown <github@muglug.com>
2019-03-04 13:37:34 -0500

    Throw a ReflectionException that can be caught by Psalm

--- src/FakeMetaCommand.php
+++ src/FakeMetaCommand.php
@@ -1,12 +1,14 @@
 <?php
-
 namespace Psalm\LaravelPlugin;
 
 class FakeMetaCommand extends \Barryvdh\LaravelIdeHelper\Console\MetaCommand
 {
-	/**
+    /**
      * @return void
      */
     protected function registerClassAutoloadExceptions() {
-	}
+        spl_autoload_register(function ($class) {
+            throw new \ReflectionException("Class '$class' not found.");
+        });
+    }
 }
