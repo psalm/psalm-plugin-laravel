d7a20ac977312191fb2bb88d4fc7f547fabadf01
Feek <fiachra@squareup.com>
2020-04-08 00:22:28 -0700

    feature: redirect return types

---
+++ src/ReturnTypeProvider/RedirectReturnTypeProvider.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace Psalm\LaravelPlugin\ReturnTypeProvider;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
+use Psalm\CodeLocation;
+use Psalm\Context;
+use Psalm\Plugin\Hook\FunctionReturnTypeProviderInterface;
+use Psalm\StatementsSource;
+use Psalm\Type;
+
+class RedirectReturnTypeProvider implements FunctionReturnTypeProviderInterface
+{
+
+    /**
+     * @return array<string>
+     */
+    public static function getFunctionIds(): array
+    {
+        return ['redirect'];
+    }
+
+    /**
+     * @param  array<PhpParser\Node\Arg> $call_args
+     *
+     * @return ?Type\Union
+     */
+    public static function getFunctionReturnType(StatementsSource $statements_source, string $function_id, array $call_args, Context $context, CodeLocation $code_location)
+    {
+        if (!$call_args) {
+            return new Type\Union([
+                new Type\Atomic\TNamedObject(Redirector::class)
+            ]);
+        }
+
+        return new Type\Union([
+            RedirectResponse::class,
+        ]);
+    }
+}
