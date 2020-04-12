70af7a7bf160922472ebb9a8c741f0fdb87a9201
Feek <fiachra@squareup.com>
2020-04-12 12:01:20 -0700

    chore: lint

--- src/FakeMetaCommand.php
+++ src/FakeMetaCommand.php
@@ -1,12 +1,15 @@
 <?php
 namespace Psalm\LaravelPlugin;
 
+use function spl_autoload_register;
+
 class FakeMetaCommand extends \Barryvdh\LaravelIdeHelper\Console\MetaCommand
 {
     /**
      * @return void
      */
-    protected function registerClassAutoloadExceptions() {
+    protected function registerClassAutoloadExceptions()
+    {
         spl_autoload_register(function (string $class) {
             throw new \ReflectionException("Class '$class' not found.");
         });
