fefe8c0eee8bcbab3abcc5be105f8a929c4c27a8
Feek <fiachra@squareup.com>
2020-05-08 18:25:26 -0700

    refactor: extract get app from boot app

--- src/ApplicationHelper.php
+++ src/ApplicationHelper.php
@@ -2,6 +2,7 @@
 
 namespace Psalm\LaravelPlugin;
 
+use Illuminate\Foundation\Application as LaravelApplication;
 use Orchestra\Testbench\Concerns\CreatesApplication;
 use function file_exists;
 use function getcwd;
@@ -11,21 +12,20 @@ final class ApplicationHelper
     use CreatesApplication;
 
     /**
-     * @return \Illuminate\Foundation\Application|\Laravel\Lumen\Application
+     * @var LaravelApplication|\Laravel\Lumen\Application|null
+     */
+    private static $app;
+
+    /**
+     * @return LaravelApplication|\Laravel\Lumen\Application
      */
     public static function bootApp()
     {
-        if (file_exists($applicationPath = __DIR__.'/../../../../bootstrap/app.php')) { // Applications
-            $app = require $applicationPath;
-        } elseif (file_exists($applicationPath = getcwd().'/bootstrap/app.php')) { // Local Dev
-            $app = require $applicationPath;
-        } else { // Packages
-            $app = (new static)->createApplication();
-        }
+        $app = self::getApp();
 
         if ($app instanceof \Illuminate\Contracts\Foundation\Application) {
             $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-        } elseif ($app instanceof \Laravel\Lumen\Application) {
+        } else {
             $app->boot();
         }
 
@@ -35,9 +35,31 @@ final class ApplicationHelper
     }
 
     /**
+     * @return LaravelApplication|\Laravel\Lumen\Application
+     */
+    public static function getApp()
+    {
+        if (self::$app) {
+            return self::$app;
+        }
+
+        if (file_exists($applicationPath = __DIR__.'/../../../../bootstrap/app.php')) { // Applications
+            $app = require $applicationPath;
+        } elseif (file_exists($applicationPath = getcwd().'/bootstrap/app.php')) { // Local Dev
+            $app = require $applicationPath;
+        } else { // Packages
+            $app = (new static)->createApplication();
+        }
+
+        self::$app = $app;
+
+        return $app;
+    }
+
+    /**
      * Resolve application bootstrapper.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param LaravelApplication $app
      *
      * @return void
      */
@@ -71,7 +93,7 @@ final class ApplicationHelper
     }
 
     /**
-     * @param \Illuminate\Foundation\Application $app
+     * @param LaravelApplication $app
      */
     protected function getEnvironmentSetUp($app): void
     {
