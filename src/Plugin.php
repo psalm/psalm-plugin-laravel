9e13fdcacdf0e6c68f5b9ba99887a2aa6a02eba5
Feek <fiachra@squareup.com>
2020-05-22 20:59:18 -0700

    fix: remove Eloquent mixin because our plugin does a better job  at the likes of find or fail

--- src/Plugin.php
+++ src/Plugin.php
@@ -71,8 +71,20 @@ class Plugin implements PluginEntryPointInterface
         $view_factory,
         string $cache_dir
     ) : void {
+        /** @var \Illuminate\Config\Repository $config */
+        $config = $app['config'];
+
+        // The \Eloquent mixin has less specific return types than our custom plugin can determine, so we unset it here
+        // to not taint our analysis
+        if ($ideHelperExtra = $config->get('ide-helper.extra')) {
+            if (isset($ideHelperExtra['Eloquent'])) {
+                unset($ideHelperExtra['Eloquent']);
+                $config->set('ide-helper.extra', $ideHelperExtra);
+            }
+        }
+
         $stubs_generator_command = new \Barryvdh\LaravelIdeHelper\Console\GeneratorCommand(
-            $app['config'],
+            $config,
             $fake_filesystem,
             $view_factory
         );
