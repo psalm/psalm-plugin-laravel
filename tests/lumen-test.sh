7a4c73042b36c4ea6c397ab71fd81cbeea5ec1a2
Feek <fiachra@squareup.com>
2020-06-21 21:31:47 -0700

    chore: run on ubuntu

--- tests/lumen-test.sh
+++ tests/lumen-test.sh
@@ -10,7 +10,7 @@ composer create-project --quiet --prefer-dist "laravel/lumen" ../lumen
 cd ../lumen/
 
 echo "Adding package from source"
-sed -e 's|"type": "project",|&"repositories": [ { "type": "path", "url": "../psalm-plugin-laravel" } ],|' -i '' composer.json
+sed -e 's|"type": "project",|&"repositories": [ { "type": "path", "url": "../psalm-plugin-laravel" } ],|' -i composer.json
 COMPOSER_MEMORY_LIMIT=-1 composer require --dev "psalm/plugin-laravel:*"
 
 echo "Analyzing Lumen"
