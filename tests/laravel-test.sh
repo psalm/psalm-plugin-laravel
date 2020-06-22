b50b4b0ca9653dc228f075ece8004db52d7e7630
Feek <fiachra@squareup.com>
2020-06-21 20:22:22 -0700

    chore: os x vs. linux sed differences

--- tests/laravel-test.sh
+++ tests/laravel-test.sh
@@ -10,7 +10,7 @@ composer create-project --quiet --prefer-dist "laravel/laravel" ../laravel
 cd ../laravel/
 
 echo "Adding package from source"
-sed -e 's|"type": "project",|&"repositories": [ { "type": "path", "url": "../psalm-plugin-laravel" } ],|' -i '' composer.json
+sed -e 's|"type": "project",|&"repositories": [ { "type": "path", "url": "../psalm-plugin-laravel" } ],|' -i composer.json
 COMPOSER_MEMORY_LIMIT=-1 composer require --dev "psalm/plugin-laravel:*"
 
 echo "Analyzing Laravel"
