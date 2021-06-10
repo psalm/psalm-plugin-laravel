d18eb313b498fc5d25e824735789804bf0b2f385
feek <5747667+mr-feek@users.noreply.github.com>
2021-06-09 22:23:33 -0700

    Revert "Update method signature for getPropertiesFromTable"

--- src/FakeModelsCommand.php
+++ src/FakeModelsCommand.php
@@ -47,7 +47,7 @@ class FakeModelsCommand extends \Barryvdh\LaravelIdeHelper\Console\ModelsCommand
      *
      * @param \Illuminate\Database\Eloquent\Model $model
      */
-    public function getPropertiesFromTable($model) : void
+    protected function getPropertiesFromTable($model) : void
     {
         $table_name = $model->getTable();
 
