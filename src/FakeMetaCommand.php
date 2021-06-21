2795bbf76fcf52231bd331e96cbd8ddee70a969a
Andrew Nagy <tm1000@users.noreply.github.com>
2021-06-21 15:47:25 -0700

    Fix compatible error
    Fixes: `Declaration of Psalm\LaravelPlugin\FakeMetaCommand::registerClassAutoloadExceptions() must be compatible with Barryvdh\LaravelIdeHelper\Console\MetaCommand::registerClassAutoloadExceptions(): callable`
    
    In https://github.com/barryvdh/laravel-ide-helper/commit/ca94ffd99a55763c5129b47c75087e132e1805db callable was added to `registerClassAutoloadExceptions`

--- src/FakeMetaCommand.php
+++ src/FakeMetaCommand.php
@@ -8,7 +8,7 @@ class FakeMetaCommand extends \Barryvdh\LaravelIdeHelper\Console\MetaCommand
     /**
      * @return void
      */
-    protected function registerClassAutoloadExceptions()
+    protected function registerClassAutoloadExceptions(): callable
     {
         // by default, the ide-helper throws exceptions when it cannot find a class. However it does not unregister that
         // autoloader when it is done, and we certainly do not want to throw exceptions when we are simply checking if
