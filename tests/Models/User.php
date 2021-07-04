402ad732442051fd8ba4a7f1c56321a178c3d229
fiachra mcdermott <fiachra@squareup.com>
2021-07-04 13:51:08 -0400

    test: add test case for scopes

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -44,4 +44,15 @@ final class User extends Model {
     {
         return $this->morphOne(Image::Class, 'imageable');
     }
+
+    /**
+     * Scope a query to only include active users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('active', 1);
+    }
 }
