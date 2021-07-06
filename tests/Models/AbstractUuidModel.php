e973fef893c7f05631aa35f1cfd9289882126dfa
Claas Augner <github@caugner.de>
2021-07-06 12:34:11 +0200

    test: ensure Model @property support

---
+++ tests/Models/AbstractUuidModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Psalm\LaravelPlugin\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * @property-read UuidInterface $uuid
+ */
+abstract class AbstractUuidModel extends Model
+{
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Model $model) {
+            $model->setAttribute('uuid', Str::uuid());
+        });
+    }
+}
