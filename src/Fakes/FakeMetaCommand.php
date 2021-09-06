40839239988279a8a5d7af539bf7434b576950f8
krasnikov87 <32748797+krasnikov87@users.noreply.github.com>
2021-09-06 09:05:31 +0300

    added return type for registerClassAutoloadExceptions

--- src/Fakes/FakeMetaCommand.php
+++ src/Fakes/FakeMetaCommand.php
@@ -9,7 +9,7 @@ class FakeMetaCommand extends MetaCommand
     /**
      * @return void
      */
-    protected function registerClassAutoloadExceptions()
+    protected function registerClassAutoloadExceptions(): callable
     {
         // by default, the ide-helper throws exceptions when it cannot find a class. However it does not unregister that
         // autoloader when it is done, and we certainly do not want to throw exceptions when we are simply checking if
