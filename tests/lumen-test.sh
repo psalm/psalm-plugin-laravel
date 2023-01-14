6c7ad5e730972d0e341b69feb631f6caa06eb12b
Alies Lapatsin <web.lapateen@gmail.com>
2023-01-14 13:47:05 +0100

    Make scripts safe to run from any directory

--- tests/lumen-test.sh
+++ tests/lumen-test.sh
@@ -2,12 +2,16 @@
 
 set -e
 
-echo "Cleaning Up"
-rm -rf ../lumen/
+CURRENT_SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+APP_INSTALLATION_PATH="$(dirname "$CURRENT_SCRIPT_PATH")/../lumen";
+
+echo "Cleaning up previous installation"
+rm -rf $APP_INSTALLATION_PATH
 
 echo "Installing Lumen"
-composer create-project laravel/lumen ../lumen 9.* --quiet --prefer-dist
-cd ../lumen/
+# See https://github.com/laravel/lumen/tags for Lumen versions
+composer create-project laravel/lumen $APP_INSTALLATION_PATH 9.* --quiet --prefer-dist
+cd $APP_INSTALLATION_PATH
 
 echo "Adding package from source"
 sed -e 's|"type": "project",|&"repositories": [ { "type": "path", "url": "../psalm-plugin-laravel" } ],|' composer.json
@@ -15,3 +19,5 @@ COMPOSER_MEMORY_LIMIT=-1 composer require --dev "psalm/plugin-laravel:*" -W
 
 echo "Analyzing Lumen"
 ./vendor/bin/psalm -c ../psalm-plugin-laravel/tests/lumen-test-psalm.xml
+
+echo "A sample Lumen application installed at the $APP_INSTALLATION_PATH directory, feel free to remove it."
