ccf1d310811e7f732d6d6b107eff1db78ad84feb
Alies Lapatsin <web.lapateen@gmail.com>
2023-01-25 11:41:13 +0100

    Support \Schema alias while parsing migrations

--- tests/Unit/Handlers/Eloquent/Schema/DefaultUserTableTest.php
+++ tests/Unit/Handlers/Eloquent/Schema/DefaultUserTableTest.php
@@ -12,14 +12,7 @@ final class DefaultUserTableTest extends AbstractSchemaAggregatorTest
     {
         $schemaAggregator = $this->instantiateSchemaAggregator(__DIR__ . '/migrations/default_users_table_anon');
 
-        $this->assertSchemaHasTableAndNotNullableColumnOfType('users.id', 'int', $schemaAggregator);
-        $this->assertSchemaHasTableAndNotNullableColumnOfType('users.email', 'string', $schemaAggregator);
-        $this->assertSchemaHasTableAndNotNullableColumnOfType('users.password', 'string', $schemaAggregator);
-        $this->assertSchemaHasTableAndNotNullableColumnOfType('users.password', 'string', $schemaAggregator);
-        $this->assertSchemaHasTableAndNotNullableColumnOfType('users.remember_token', 'string', $schemaAggregator);
-        $this->assertSchemaHasTableAndNullableColumnOfType('users.email_verified_at', 'string', $schemaAggregator);
-        $this->assertSchemaHasTableAndNullableColumnOfType('users.created_at', 'string', $schemaAggregator);
-        $this->assertSchemaHasTableAndNullableColumnOfType('users.updated_at', 'string', $schemaAggregator);
+        $this->assertAllDefaultUsersTableColumnsDetectedProperly($schemaAggregator);
     }
 
     /** @test */
@@ -27,6 +20,19 @@ final class DefaultUserTableTest extends AbstractSchemaAggregatorTest
     {
         $schemaAggregator = $this->instantiateSchemaAggregator(__DIR__ . '/migrations/default_users_table_named');
 
+        $this->assertAllDefaultUsersTableColumnsDetectedProperly($schemaAggregator);
+    }
+
+    /** @test */
+    public function it_detects_all_columns_from_migration_that_uses_root_namespace_facades(): void
+    {
+        $schemaAggregator = $this->instantiateSchemaAggregator(__DIR__ . '/migrations/default_users_table_root_ns_facade');
+
+        $this->assertAllDefaultUsersTableColumnsDetectedProperly($schemaAggregator);
+    }
+
+    private function assertAllDefaultUsersTableColumnsDetectedProperly(SchemaAggregator $schemaAggregator): void
+    {
         $this->assertSchemaHasTableAndNotNullableColumnOfType('users.id', 'int', $schemaAggregator);
         $this->assertSchemaHasTableAndNotNullableColumnOfType('users.email', 'string', $schemaAggregator);
         $this->assertSchemaHasTableAndNotNullableColumnOfType('users.password', 'string', $schemaAggregator);
