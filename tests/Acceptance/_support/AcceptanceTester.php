a478b0c5ed85738567e9ccdf8424fee1488d05d3
Alies Lapatsin <web.lapateen@gmail.com>
2023-02-01 23:19:44 +0100

    Cleaner tests structure (organize by type)

--- tests/Acceptance/_support/AcceptanceTester.php
+++ tests/Acceptance/_support/AcceptanceTester.php
@@ -14,12 +14,12 @@
  * @method void pause()
  *
  * @SuppressWarnings(PHPMD)
-*/
+ */
 class AcceptanceTester extends \Codeception\Actor
 {
     use _generated\AcceptanceTesterActions;
 
-   /**
-    * Define custom actions here
-    */
+    /**
+     * Define custom actions here
+     */
 }
