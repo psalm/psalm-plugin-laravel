82de0e3b8f4b3f54fd7455169e9b3c68e91d8951
Alies Lapatsin <web.lapateen@gmail.com>
2025-02-02 14:25:11 +0100

    Bump dependencies: drop Laravel 10 support

--- rector.php
+++ rector.php
@@ -2,7 +2,6 @@
 
 use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
 use Rector\CodingStyle\Rector\Assign\SplitDoubleAssignRector;
-use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\CodingStyle\Rector\If_\NullableCompareToNullRector;
 use Rector\Config\RectorConfig;
@@ -11,24 +10,19 @@ use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php81\Rector\Array_\FirstClassCallableRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Strict\Rector\BooleanNot\BooleanInBooleanNotRuleFixerRector;
-use Rector\Strict\Rector\If_\BooleanInIfConditionRuleFixerRector;
 
 return RectorConfig::configure()
     ->withPaths(['src', 'tests'])
-    ->withPhpSets(php81: true)
+    ->withPhpSets(php82: true)
     ->withPreparedSets(deadCode: true, codingStyle: true, typeDeclarations: true)
     ->withSkip([
         ReadOnlyPropertyRector::class,
         ClosureToArrowFunctionRector::class,
         FirstClassCallableRector::class,
         NullToStrictStringFuncCallArgRector::class,
-        BooleanInIfConditionRuleFixerRector::class,
-        BooleanInBooleanNotRuleFixerRector::class,
         RemoveUnusedPrivateMethodRector::class,
         SimplifyUselessVariableRector::class,
         NullableCompareToNullRector::class,
         EncapsedStringsToSprintfRector::class,
-        StaticClosureRector::class,
         SplitDoubleAssignRector::class,
     ]);
