a29d2c5eafa28fe43e4e5937f36b90fad1cb41c1
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2025-06-04 15:32:15 +0200

    feat: configurable stub cache via LARAVEL_PLUGIN_CACHE_PATH

--- src/Providers/CacheDirectoryProvider.php
+++ src/Providers/CacheDirectoryProvider.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace Psalm\LaravelPlugin\Providers;
 
+use function getenv;
+use function rtrim;
+
 final class CacheDirectoryProvider
 {
     public static function getCacheLocation(): string
     {
+        $env = getenv('LARAVEL_PLUGIN_CACHE_PATH');
+        if ($env !== false && $env !== '') {
+            return rtrim($env, '/');
+        }
+
         return __DIR__ . '/../../cache';
     }
 }
