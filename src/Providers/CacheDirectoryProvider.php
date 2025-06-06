4609431502eb244379538373367a6032c5c80cce
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2025-06-06 09:06:00 +0200

    refactor: rename env var to PSALM_LARAVEL_PLUGIN_CACHE_PATH for better clarity

--- src/Providers/CacheDirectoryProvider.php
+++ src/Providers/CacheDirectoryProvider.php
@@ -7,15 +7,17 @@ namespace Psalm\LaravelPlugin\Providers;
 use function getenv;
 use function rtrim;
 
+use const DIRECTORY_SEPARATOR;
+
 final class CacheDirectoryProvider
 {
     public static function getCacheLocation(): string
     {
-        $env = getenv('LARAVEL_PLUGIN_CACHE_PATH');
+        $env = getenv('PSALM_LARAVEL_PLUGIN_CACHE_PATH');
         if ($env !== false && $env !== '') {
-            return rtrim($env, '/');
+            return rtrim($env, DIRECTORY_SEPARATOR);
         }
 
-        return __DIR__ . '/../../cache';
+        return __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'cache';
     }
 }
