2a5eb1a80b00ce9fde28351cc625ab0e9525c958
Viacheslav Danilin <viacheslavdanilin@gmail.com>
2025-06-06 09:06:00 +0200

    refactor: rename env var to PSALM_LARAVEL_PLUGIN_CACHE_PATH for better clarity

--- tests/Unit/Providers/CacheDirectoryProviderTest.php
+++ tests/Unit/Providers/CacheDirectoryProviderTest.php
@@ -10,25 +10,27 @@ use Psalm\LaravelPlugin\Providers\CacheDirectoryProvider;
 use function putenv;
 use function sys_get_temp_dir;
 
+use const DIRECTORY_SEPARATOR;
+
 final class CacheDirectoryProviderTest extends TestCase
 {
     protected function tearDown(): void
     {
-        putenv('LARAVEL_PLUGIN_CACHE_PATH');
+        putenv('PSALM_LARAVEL_PLUGIN_CACHE_PATH');
     }
 
     public function testReturnsDefaultPathIfEnvNotSet(): void
     {
         $path = CacheDirectoryProvider::getCacheLocation();
 
-        $this->assertStringEndsWith('/cache', $path);
+        $this->assertStringEndsWith(DIRECTORY_SEPARATOR . 'cache', $path);
         $this->assertFileExists($path);
     }
 
     public function testReturnsCustomPathFromEnv(): void
     {
         $custom = sys_get_temp_dir() . '/psalm-laravel-stubs-test';
-        putenv("LARAVEL_PLUGIN_CACHE_PATH={$custom}");
+        putenv("PSALM_LARAVEL_PLUGIN_CACHE_PATH={$custom}");
 
         $this->assertSame($custom, CacheDirectoryProvider::getCacheLocation());
     }
