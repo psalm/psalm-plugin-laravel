1eb96206a3aeb34eb9ce251e8f784aaf11f6dbd3
Alies Lapatsin <web.lapateen@gmail.com>
2026-02-24 22:31:59 +0100

    Add StringClassNameToClassConstantRector and remove FirstClassCallableRector

--- rector.php
+++ rector.php
@@ -6,8 +6,8 @@ use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\CodingStyle\Rector\If_\NullableCompareToNullRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
-use Rector\Php81\Rector\Array_\FirstClassCallableRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 
 return RectorConfig::configure()
@@ -16,11 +16,11 @@ return RectorConfig::configure()
     ->withPreparedSets(deadCode: true, codingStyle: true, typeDeclarations: true, codeQuality: true)
     ->withSkip([
         ClosureToArrowFunctionRector::class,
-        FirstClassCallableRector::class,
         NullToStrictStringFuncCallArgRector::class,
         RemoveUnusedPrivateMethodRector::class,
         SimplifyUselessVariableRector::class,
         NullableCompareToNullRector::class,
         EncapsedStringsToSprintfRector::class,
         SplitDoubleAssignRector::class,
+        StringClassNameToClassConstantRector::class, // analyzed classes are not always auto-loaded
     ]);
