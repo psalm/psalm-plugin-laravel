c26969c68ba248f09d1c0d59b1006df4f01f30c4
Alies Lapatsin <web.lapateen@gmail.com>
2026-03-27 23:44:03 +0100

    style: use imported Union instead of FQCN #0

--- src/Handlers/Validation/ValidationRuleAnalyzer.php
+++ src/Handlers/Validation/ValidationRuleAnalyzer.php
@@ -132,7 +132,7 @@ final class ValidationRuleAnalyzer
             // Type-bearing rules (first one wins for type, all accumulate taint)
             $ruleType = self::ruleToType($ruleName, $ruleParam);
 
-            if ($ruleType instanceof \Psalm\Type\Union && !$type instanceof \Psalm\Type\Union) {
+            if ($ruleType instanceof Union && !$type instanceof Union) {
                 $type = $ruleType;
             }
 
